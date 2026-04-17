67214f77ae7cce8f72d910c7fa97539a4f3f618d
Kevin <schmutz.k22@hotmail.ch>
2026-04-18 00:04:56 +0200

    E.T course 13/20

--- eth_course/02_celsius_to_fahrenheit_calculator.py
+++ eth_course/02_celsius_to_fahrenheit_calculator.py
@@ -1,2 +1,6 @@
-x = float(input("Aktuelle Temperatur in Celsius eingeben:\n"))
-# 
+celsius = float(input("Enter temperature in Celsius:\n"))
+# The user is asked to enter a temperature in Celsius, which is then stored as a float in the variable "celsius".
+fahrenheit = 9 * celsius / 5 + 32
+# The formula to convert celsius to fahrenheit is applied: (9/5) * celsius + 32, and the result is stored in the variable "fahrenheit".
+print("Temperature in Fahrenheit:", fahrenheit)
+# The calculated temperature in Fahrenheit is printed to the console.
---
+++ eth_course/03_work_with_stings.py
@@ -0,0 +1,7 @@
+home = "France"
+# We define a variable "home" and assign it the string value "France".
+guest = "Switzerland"
+# We define another variable "guest" and assign it the string value "Switzerland".
+print(f"First match:\n{home} vs {guest}")
+# Prints the string "First match:" followed by a newline, and then the values of the variables "home" and "guest" formatted as a match (e.g., "France vs Switzerland"). The curly brackets {} are used to indicate the variables to be printed.
+print(home[0],home[1],home[2], ":" , guest[0], guest[1], guest[2])
