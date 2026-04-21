99e296aa2a5945ffab1237c7fa65976fde0050cd
Kevin Schmutz <kevinschmutz@MacBook-Pro-von-Kevin.local>
2026-04-21 21:50:50 +0200

    Update ATM LOOP?

--- eth_course/05_ATM.py
+++ eth_course/05_ATM.py
@@ -2,6 +2,9 @@ print("WELCOME TO THE BANK YOU TRUST")
 x = float(input("How much CHF. do you want to withdraw? "))
 print(f"You want to withdraw {x:.2f} CHF\nPlease wait...")
 
+if x > 1000: 
+    print("The amount of cash that can be withdrawn is max. 1000 CHF at this ATM. Please try another amount.\n")
+    continue
 hundreds = x // 100
 x = x % 100
 fifthies = x // 50
@@ -10,4 +13,5 @@ twenties = x // 20
 x = x % 20
 tens = x // 10
 x = x % 10
+if x > 0: print(f"The rest value of {x:.2f} can not be given. 10 notes are the lowest possible amount that can given")
 print(f"100 notes: {hundreds:,.0f}\n50 notes: {fifthies:,.0f}\n20 notes: {twenties:,.0f}\n10 notes: {tens:,.0f}\nRest: {x:.2f} CHF")
