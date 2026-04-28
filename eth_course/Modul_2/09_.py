f9ddce2b05a6f3b7e1b2a2db65d01ef678549180
Kevin <schmutz.k22@hotmail.ch>
2026-04-28 22:11:32 +0200

    Modul2.2

--- eth_course/Modul_2/09_.py
+++ eth_course/Modul_2/09_.py
@@ -1,2 +1,9 @@
-x = 6
-print(x > 3 and x < 5)
+print("Please enter your 3-digit pin code.")
+x = int(input("1. "))
+y = int(input("2. "))
+z = int(input("3. "))
+
+if x == 0 and y == 0 and z == 7:
+    print(f"You've entered: {x}{y}{z}")
+else:
+    print("WRONG PIN")
